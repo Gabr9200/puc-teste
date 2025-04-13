8d6cc10511971ca503970bac76a0d299b9e542e4
Gabr9200 <gabrielbley92@gmail.com>
2025-04-13 16:07:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+@app.get("funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
 
