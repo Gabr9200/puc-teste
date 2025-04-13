733195446c4f4dc4af31ed8261ee957bfef2fd6e
Gabr9200 <gabrielbley92@gmail.com>
2025-04-13 16:28:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#127.0.0.1:8000/
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+#127.0.0.1:8000/teste
+@app.get("/teste")
 async def funcaoteste():
     return {"teste": "deu certo"}
 
